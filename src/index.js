266b4ceb71a300d4d24751e059a4f6eee6c96171
segodimo <segodimo@gmail.com>
2020-01-29 11:58:58 -0300

    conectando com sql

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log('OKOKOKOK');
+const app = require('./app');
+
+async function main() {
+    await app.listen(4000);
+    console.log('Conectado na porta 4000 => OK OK OK');
+}
+
+main();
